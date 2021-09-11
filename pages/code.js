4b4c2e6eeacb39d087772224410bdf9528a80f32
Luis Felipe <lipe.lftn@gmail.com>
2021-09-10 23:13:37 -0300

    montando logica

--- pages/code.js
+++ pages/code.js
@@ -1,21 +1,40 @@
-
 setInterval(
     function carregar(){
+        
         let texto = document.getElementById('texto')
    
         let data = new Date();
         let hora = data.getHours();
         let minuto = data.getMinutes();
         let segundo = data.getSeconds();
+        let dia = data.getUTCDay();
+        let valorInicial;
+        let soma;
+        let valorAtual;
         //zero
         hora = zero(hora)
         minuto = zero(minuto)
         segundo = zero(segundo)
         //
-        texto.innerHTML = `${hora}:${minuto}:${segundo}`
+        if(dia==7 && hora==15 && minuto==30 && segundo == 0){
+            valorSegundos == 604800;
+            valorAtual
+            
+        }
+        else if(dia==6 && hora==23 && minuto==04){
+            valorInicial == 58800
+            texto.innerHTML = `${diminuir(valorInicial)}`
+        }
+        
         
     
 }, 1000); 
+
+function diminuir(x){
+    x = x-1;
+} return x
+
+
  function zero(x){
      if(x<10){
          x='0'+x;
